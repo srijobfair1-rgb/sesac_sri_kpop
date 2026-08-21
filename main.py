f637a4e9772a301b8a6b085121be7ca3d92f138d
srijobfair1-rgb <sri.jobfair1@gmail.com>
2026-08-21 13:50:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ def get_saramin_jobs(group_name, job_cd_string, loc_cd, exp_cd, api_key):
         "loc_cd": loc_cd,             
         "experience_level": exp_cd,   
         "sort": "pd",                 
-        "count": "110"                
+        "count": "110"
+        "sr": "directhire"
     }
     
     jobs_collected = []
